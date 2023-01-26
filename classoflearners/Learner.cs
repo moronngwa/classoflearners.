6110427dfd8e9ac3787bc9b1b2a655329706d412
MORONGWA <104618822+moronngwa@users.noreply.github.com>
2023-01-26 10:48:03 +0200

    Add project files.

---
+++ classoflearners/Learner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classoflearners
+{
+    class Learners
+    {
+        public string firstname;
+        public string lastname;
+        public string gender;
+        public int age;
+        public char initals;
+
+
+        public Learners(string Firstname, string Lastname, string Gender, int Age, char Initials)
+
+        {
+            Fisrtname = Firstname;
+            lastname = Lastname;
+            gender = Gender;
+            age = Age;
+            initals = Initials;
+        }
+
+    }
+}
+
---
+++ classoflearners/Program.cs
@@ -0,0 +1,10 @@
+﻿using classoflearners;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Learners learner1 = new Learners("Morongwa", "Mathipa", "Female", 30, 'M');
+        Console.WriteLine(learner1.firstname + "" + learner1.lastname + "" + learner1.gender + "" + learner1.age + "" + learner1.initals);
+    }
+}
